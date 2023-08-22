ec0fd5e659d670be67d47a73b7835c7037460423
Daniil <oleinik.d_78@mail.ru>
2023-08-22 17:30:17 +0300

    task_10 done

--- Task_10.py
+++ Task_10.py
@@ -3,3 +3,29 @@
 # Определите минимальное число монеток, которые нужно перевернуть, 
 # чтобы все монетки были повернуты вверх одной и той же стороной. 
 # Выведите минимальное количество монет, которые нужно перевернуть
+
+from random import getrandbits
+
+def console_input():
+    coll=int(input("Сколько монеток было подбросить?: "))
+    return coll
+
+def flipping_coins():
+    n = console_input()
+    i=1
+    count_of_head = 0
+    count_of_tail = 0
+    while i <=n:
+        coin = getrandbits(1)
+        print(coin)
+        if coin == 0:
+            count_of_head += 1
+        else:
+            count_of_tail += 1
+        i += 1
+    if count_of_tail > count_of_head:
+        return count_of_head
+    else:
+        return count_of_tail
+
+print(flipping_coins())
